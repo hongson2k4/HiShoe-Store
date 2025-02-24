0ef8fbe6bfde3d0724c4ce0ddf942b68c288a376
Gia Huy <huytqgph43005@fpt.edu.vn>
2025-02-25 00:35:30 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -76,5 +76,5 @@ Route::middleware(['admin'])->controller(ProductsController::class)
         Route::get('/edit/{id}', [ProductsController::class, 'edit'])->where('id', '[0-9]+')->name('edit');
         Route::put('/update/{id}', [ProductsController::class, 'updateProduct'])->where('id', '[0-9]+')->name('update');
         Route::delete('destroy/{id}', [ProductsController::class, 'destroyProduct'])->where('id', '[0-9]+')->name('destroy');
-    })aaaa
+    })
 ;

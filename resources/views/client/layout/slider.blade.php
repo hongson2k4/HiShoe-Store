22f523bc0a8cf7c3688b5c8eab94d43b32487ba4
hongson2k4 <hongson2k4xx@gmail.com>
2025-05-28 21:34:50 +0700

    cập nhật giao diệndiện

--- resources/views/client/layout/slider.blade.php
+++ resources/views/client/layout/slider.blade.php
@@ -1,94 +1,100 @@
 <section class="slider_section">
-      <div class="slider_container">
+    <div class="slider_container">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <div class="container-fluid">
-                <div class="row">
-                  <div class="col-md-7">
-                    <div class="detail-box">
-                      <h1>
-                        Welcome To Our <br>
-                        Gift Shop
-                      </h1>
-                      <p>
-                        Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non necessitatibus error distinctio mollitia suscipit. Nostrum fugit doloribus consequatur distinctio esse, possimus maiores aliquid repellat beatae cum, perspiciatis enim, accusantium perferendis.
-                      </p>
-                      <a href="">
-                        Contact Us
-                      </a>
+            <div class="carousel-inner">
+                <!-- Slide 1 -->
+                <div class="carousel-item active">
+                    <div class="container-fluid position-relative" style="overflow:hidden;">
+                        <img src="{{Storage::url('uploads/images/6.webp')}}" alt="HiShoe Store"
+                            style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;z-index:1;" />
+                        <div class="row position-relative" style="z-index:2;">
+                            <div class="col-md-7">
+                                <div class="detail-box">
+                                    <h1>
+                                        Chào mừng đến với <br>
+                                        HiShoe Store
+                                    </h1>
+                                    <p>
+                                        Khám phá bộ sưu tập giày mới nhất với nhiều ưu đãi hấp dẫn. Chất lượng đảm bảo,
+                                        mẫu mã đa dạng, phục vụ tận tâm cho mọi khách hàng.
+                                    </p>
+                                    <a href="">
+                                        Liên hệ với chúng tôi
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-5 ">
+                                <!-- Có thể bỏ img-box này nếu không muốn ảnh lặp lại -->
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                  <div class="col-md-5 ">
-                    <div class="img-box">
-                      <img src="images/slider-img.png" alt="" />
-                    </div>
-                  </div>
                 </div>
-              </div>
-            </div>
-            <div class="carousel-item ">
-              <div class="container-fluid">
-                <div class="row">
-                  <div class="col-md-7">
-                    <div class="detail-box">
-                      <h1>
-                        Welcome To Our <br>
-                        Gift Shop
-                      </h1>
-                      <p>
-                        Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non necessitatibus error distinctio mollitia suscipit. Nostrum fugit doloribus consequatur distinctio esse, possimus maiores aliquid repellat beatae cum, perspiciatis enim, accusantium perferendis.
-                      </p>
-                      <a href="">
-                        Contact Us
-                      </a>
+                <!-- Slide 2 -->
+                <div class="carousel-item ">
+                    <div class="container-fluid position-relative" style="overflow:hidden;">
+                        <img src="{{Storage::url('uploads/images/6.webp')}}" alt="Ưu đãi HiShoe"
+                            style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;z-index:1;" />
+                        <div class="row position-relative" style="z-index:2;">
+                            <div class="col-md-7">
+                                <div class="detail-box">
+                                    <h1>
+                                        Ưu đãi đặc biệt <br>
+                                        Dành cho bạn
+                                    </h1>
+                                    <p>
+                                        Đừng bỏ lỡ các chương trình khuyến mãi và mã giảm giá hấp dẫn chỉ có tại HiShoe
+                                        Store. Mua sắm ngay hôm nay để nhận nhiều phần quà giá trị!
+                                    </p>
+                                    <a href="">
+                                        Xem ưu đãi
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-5 ">
+                                <!-- Có thể bỏ img-box này nếu không muốn ảnh lặp lại -->
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                  <div class="col-md-5 ">
-                    <div class="img-box">
-                      <img src="images/slider-img.png" alt="" />
-                    </div>
-                  </div>
                 </div>
-              </div>
-            </div>
-            <div class="carousel-item ">
-              <div class="container-fluid">
-                <div class="row">
-                  <div class="col-md-7">
-                    <div class="detail-box">
-                      <h1>
-                        Welcome To Our <br>
-                        Gift Shop
-                      </h1>
-                      <p>
-                        Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non necessitatibus error distinctio mollitia suscipit. Nostrum fugit doloribus consequatur distinctio esse, possimus maiores aliquid repellat beatae cum, perspiciatis enim, accusantium perferendis.
-                      </p>
-                      <a href="">
-                        Contact Us
-                      </a>
+                <!-- Slide 3 -->
+                <div class="carousel-item ">
+                    <div class="container-fluid position-relative" style="overflow:hidden;">
+                        <img src="{{Storage::url('uploads/images/6.webp')}}" alt="Sản phẩm HiShoe"
+                            style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;z-index:1;" />
+                        <div class="row position-relative" style="z-index:2;">
+                            <div class="col-md-7">
+                                <div class="detail-box">
+                                    <h1>
+                                        Sản phẩm chất lượng <br>
+                                        Phục vụ tận tâm
+                                    </h1>
+                                    <p>
+                                        HiShoe Store cam kết mang đến cho bạn những sản phẩm tốt nhất cùng dịch vụ chăm
+                                        sóc khách hàng chuyên nghiệp, tận tình.
+                                    </p>
+                                    <a href="">
+                                        Xem sản phẩm
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-5 ">
+                                <!-- Có thể bỏ img-box này nếu không muốn ảnh lặp lại -->
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                  <div class="col-md-5 ">
-                    <div class="img-box">
-                      <img src="images/slider-img.png" alt="" />
-                    </div>
-                  </div>
                 </div>
-              </div>
             </div>
-          </div>
-          <div class="carousel_btn-box">
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-              <i class="fa fa-arrow-left" aria-hidden="true"></i>
-              <span class="sr-only">Previous</span>
-            </a>
-            <img src="images/line.png" alt="" />
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-              <i class="fa fa-arrow-right" aria-hidden="true"></i>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
+            <div class="carousel_btn-box">
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <i class="fa fa-arrow-left" aria-hidden="true"></i>
+                    <span class="sr-only">Trước</span>
+                </a>
+                <img src="images/line.png" alt="" />
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                    <span class="sr-only">Tiếp</span>
+                </a>
+            </div>
         </div>
-      </div>
-    </section>
+    </div>
+</section>
